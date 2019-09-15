8246eee82e8fde30e302b5bb613308e8d5506ae0
hk1510 <hkthetinkerer@gmail.com>
2019-09-14 20:51:02 -0700

    commit

---
+++ CheckpointExercises/src/homework2/MonsterAttack.java
@@ -0,0 +1,38 @@
+package homework2;
+
+import java.util.Date;
+
+public class MonsterAttack {
+	public String monsterName;
+	public String attackLocation;
+	public double damagesInMillionUSD;
+	public Date date;
+	
+	public String getMonsterName() {
+		return monsterName;
+	}
+	public String getAttackLocation() {
+		return attackLocation;
+	}
+	public double getDamagesInMillionUSD() {
+		return damagesInMillionUSD;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setMonsterName(String monsterName) {
+		this.monsterName = monsterName;
+	}
+	public void setAttackLocation(String attackLocation) {
+		this.attackLocation = attackLocation;
+	}
+	public void setDamagesInMillionUSD(double damagesInMillionUSD) {
+		this.damagesInMillionUSD = damagesInMillionUSD;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String toString() {
+		return monsterName + " caused " + damagesInMillionUSD + " million dollars of damage at " + attackLocation + " on " + date.getMonth() + "/" + date.getDay() + "/" + date.getYear();
+	}
+}
